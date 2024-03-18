625d4ef6785fd12f9f202be20359119502d4026b
Konstantin Vasilev <burmuley@gmail.com>
2024-03-18 09:50:46 -0700

    v0.1.1
    Fixed dump type in example.json and extended structure with more examples
    Now parsing default value and rendering it right into a go code depending on the data type
    Minor templates fixes

--- schema.go
+++ schema.go
@@ -1,15 +1,79 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 )
 
+type VariableType string
+
+func (vt *VariableType) UnmarshalJSON(b []byte) error {
+	{ // try json.Number
+		var n json.Number
+		if err := json.Unmarshal(b, &n); err == nil {
+			*vt = VariableType(n.String())
+			return nil
+		}
+	}
+
+	{ // try string
+		var s string
+		if err := json.Unmarshal(b, &s); err == nil {
+			*vt = VariableType(fmt.Sprintf("%q", s))
+			return nil
+		}
+	}
+
+	{ // try string
+		var bs bool
+		if err := json.Unmarshal(b, &bs); err == nil {
+			*vt = VariableType(fmt.Sprintf("%t", bs))
+			return nil
+		}
+	}
+
+	{ // try map
+		if b[0] == '{' {
+			var m map[string]VariableType
+			if err := json.Unmarshal(b, &m); err == nil {
+				mb := bytes.NewBuffer([]byte{})
+				mb.WriteString("{\n")
+				for k, v := range m {
+					mb.WriteString(fmt.Sprintf("%q: %v,\n", k, v))
+				}
+				mb.WriteString("}")
+				*vt = VariableType(mb.String())
+				return nil
+			}
+		}
+	}
+
+	{ // try array
+		if b[0] == '[' {
+			var a []VariableType
+			if err := json.Unmarshal(b, &a); err == nil {
+				arrb := bytes.NewBuffer([]byte{})
+				arrb.WriteString("{\n")
+				for _, v := range a {
+					arrb.WriteString(fmt.Sprintf("%v,\n", v))
+				}
+				arrb.WriteString("}")
+				*vt = VariableType(arrb.String())
+				return nil
+			}
+		}
+	}
+
+	return errors.New("unknown type of default value")
+}
+
 type FieldParameters struct {
-	Type     string `json:"type"`
-	Required bool   `json:"required"`
-	Default  any    `json:"default"`
+	Type     string       `json:"type"`
+	Required bool         `json:"required"`
+	Default  VariableType `json:"default"`
 }
 
 type Fields map[string]FieldParameters
